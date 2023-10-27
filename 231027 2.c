1552f2c608893b4018ef11cf75bc2fd2c7d3d732
waonn777 <148538251+waonn777@users.noreply.github.com>
2023-10-27 19:32:57 +0900

    Add files via upload

---
+++ 231027 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("n‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+	scanf_s("%d", &n);
+	if (n % 2 == 0)
+	{
+		printf("‹ô”‚Å‚·\n");
+	}
+	else
+	{
+		printf("Šï”‚Å‚·\n");
+	}
+	return 0;
+}
